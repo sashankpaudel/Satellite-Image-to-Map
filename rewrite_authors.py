42c9527c36c834546a4bc73fcfa0011f1e62df42
Sashank <sashankpaudel07@gmail.com>
2025-11-15 16:48:51 +0545

    s

---
+++ rewrite_authors.py
@@ -0,0 +1,9 @@
+def name_callback(name):
+    if name == b"Shashikant Prasad":
+        return b"Sashank Paudel"
+    return name
+
+def email_callback(email):
+    if email == b"shashikantprasad1111@gmail.com":
+        return b"sashankpaudel07@gmail.com"
+    return email
